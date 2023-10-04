3196dc4917116e9d3a684d08b28dd81360ec76ba
dllido <dllido@uji.es>
2023-10-04 16:47:36 +0200

    oct04

--- PHP/ejTeo/T3/session.php
+++ PHP/ejTeo/T3/session.php
@@ -1,32 +1,21 @@
 <?php
-// Initialize the session.
-// If you are using session_name("something"), don't forget it now!
-print "Cookies";
-var_dump($_COOKIE);
-print "Session";
-var_dump($_SESSION);
+$session_name("MiprimeraSesi");
 session_start();
-print("session_name:" . sessionName());
-print "Cookies";
-var_dump($_COOKIE);
-var_dump($_SESSION);
-var_dump($_GLOBALS);
-
+print "<p>Cookies:</p>";
+print_r($_COOKIE);
+print ("<p>Session:".session_name()."</p>");
+print_r($_SESSION);
 if (!isset($_SESSION["activo"])) {
-    $_SESSION = array();
-    setcookie(session_name(), '', time() + 10);
     $_SESSION["activo"] = 1;
     print "<h2>Hola</h2>";
     $_SESSION["usuario"] = "visitante";
+    setcookie(session_name(), '', time() + 10);
 } else {
     if ($_SESSION['last_action'] < time() - 60 /* be a little tolerant here */) {
-        session_destroy();
-    } // destroy the session and quit
-
-    print "<h2>bienvenido de nuevo visitante\n</H2>";
+        session_destroy();}
+    echo "<H2>bienvenido de nuevo ", $_SESSION["usuario"],"</H2>";
 }
-print "Session";
-var_dump($_SESSION);
-print "Cookies";
-var_dump($_COOKIE);
+print "<p>SessionF:</p>";
+print_r($_SESSION);
+print_r($_COOKIE);
 ?>
--- PHP/ejTeo/T3/session0.php
+++ PHP/ejTeo/T3/session0.php
@@ -1,18 +1,18 @@
 <?php
+$session_name("MiprimeraSesi");
 session_start();
 print "<p>Cookies:</p>";
 var_dump($_COOKIE);
-$sn=session_name()
-print("<p>Session: $sn </p>");
+print ("<p>Session:".session_name()."</p>");
 var_dump($_SESSION);
 if (!isset($_SESSION["activo"])) {
-    $_SESSION = array();
     $_SESSION["activo"] = 1;
     print "<h2>Hola</h2>";
     $_SESSION["usuario"] = "visitante";
 } else {
-    echo "<H2>bienvenido de nuevo ", $_SESSION["usuario"], "</H2>";
+    echo "<H2>bienvenido de nuevo ", $_SESSION["usuario"],"</H2>";
 }
 print "<p>SessionF:</p>";
 var_dump($_SESSION);
+var_dump($_COOKIE);
 ?>
